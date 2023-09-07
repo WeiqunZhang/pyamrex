c7f8b616fbe2af59a34b74f298a853ac313a8613
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-09-06 21:08:45 -0700

    rm ccache

--- .github/workflows/dependencies/dependencies_ccache.sh
+++ .github/workflows/dependencies/dependencies_ccache.sh
@@ -8,4 +8,5 @@ fi
 
 wget https://github.com/ccache/ccache/releases/download/v${CVER}/ccache-${CVER}-linux-x86_64.tar.xz
 tar xvf ccache-${CVER}-linux-x86_64.tar.xz
-sudo cp -f ccache-${CVER}-linux-x86_64/ccache /usr/local/bin/
+sudo mv -f ccache-${CVER}-linux-x86_64/ccache /usr/local/bin/
+sudo rm -f ccache-${CVER}-linux-x86_64.tar.xz
